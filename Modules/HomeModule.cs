12ff07fdbeb18cbcd17f73ed2f2234198943bb4e
Russ Davies <russdavies392@gmail.com>
2016-08-05 15:25:01 -0700

    add html for viewing current bands and venues

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,6 +14,14 @@ namespace BandTracker
         return View["index.cshtml"];
       };
 
+      Get["/bands"] = _ => {
+        List<Band> AllBands = Band.GetAll();
+        return View["bands.cshtml", AllBands];
+      };
+      Get["/venues"] = _ => {
+        List<Venue> AllVenues = Venue.GetAll();
+        return View["venues.cshtml", AllVenues];
+      };
     }
   }
 }
