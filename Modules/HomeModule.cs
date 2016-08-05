2afe8d15f69eaee48f907b7d29b64af621f71ce6
Russ Davies <russdavies392@gmail.com>
2016-08-05 16:08:43 -0700

    add all html and nancy

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -79,6 +79,28 @@ namespace BandTracker
         venue.AddBand(band);
         return View["success.cshtml"];
       };
+
+      Get["venue/edit/{id}"] = parameters => {
+       Venue SelectedVenue = Venue.Find(parameters.id);
+       return View["venue_edit.cshtml", SelectedVenue];
+      };
+
+      Patch["venue/edit/{id}"] = parameters => {
+       Venue SelectedVenue = Venue.Find(parameters.id);
+       SelectedVenue.Update(Request.Form["venue-name"]);
+       return View["success.cshtml"];
+      };
+
+      Get["venue/delete/{id}"] = parameters => {
+       Venue SelectedVenue =Venue.Find(parameters.id);
+       return View["venue_delete.cshtml", SelectedVenue];
+      };
+
+      Delete["venue/delete/{id}"] = parameters => {
+       Venue SelectedVenue =Venue.Find(parameters.id);
+       SelectedVenue.Delete();
+       return View["success.cshtml"];
+      };
     }
   }
 }
