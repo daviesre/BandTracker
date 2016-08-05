4668daaa678eec81e3b4c37b04e776d28dc37f8f
Russ Davies <russdavies392@gmail.com>
2016-08-05 15:19:56 -0700

    add basic nancy framework

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+
+
+namespace BandTracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ =>
+      {
+        return View["index.cshtml"];
+      };
+
+    }
+  }
+}
